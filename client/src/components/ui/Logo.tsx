8af4bcb3e9b5afc9dba7d3de420b15d6f542c595
Andrei Pavlov <andrpav9@gmail.com>
2025-08-15 09:48:01 +0400

    feat: add logo ui component and style it

--- client/src/components/ui/Logo.tsx
+++ client/src/components/ui/Logo.tsx
@@ -3,8 +3,19 @@ import { Link } from 'react-router-dom';
 
 export const Logo: FC = () => {
   return (
-    <div>
-      <Link to="/">Logo</Link>
-    </div>
+    <Link to="/">
+      <div className="flex">
+        <div className="rounded-full py-0.5 px-2 bg-gradient-to-r from-gray-800 to-gray-300">
+          <span className="text-gray-100 text-xl md:text-2xl font-semibold">
+            Solo
+          </span>
+        </div>
+        <div className="rounded-full py-0.5 px-2 bg-gradient-to-l from-gray-300 to-gray-100 ml-[-8px]">
+          <span className="text-gray-900 text-xl md:text-2xl font-semibold">
+            Write
+          </span>
+        </div>
+      </div>
+    </Link>
   );
 };
