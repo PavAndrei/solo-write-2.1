27367b0ce0fe466fb239763cb9c0b72a3109140d
Andrei Pavlov <andrpav9@gmail.com>
2025-08-16 20:54:09 +0400

    feat: add the title to the editro page

--- client/src/pages/Editor.tsx
+++ client/src/pages/Editor.tsx
@@ -1,5 +1,15 @@
 import type { FC } from 'react';
+import { Container } from '../components/layout/Container';
+import { PageTitle } from '../components/ui/PageTitle';
 
 export const Editor: FC = () => {
-  return <section>editor</section>;
+  return (
+    <section className="h-full py-10">
+      <Container>
+        <PageTitle hasSubtitle="Do not miss the opportunity to share your views with the others">
+          Write an article
+        </PageTitle>
+      </Container>
+    </section>
+  );
 };
